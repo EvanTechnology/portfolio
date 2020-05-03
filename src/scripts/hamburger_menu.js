8656bf86f5a50daf16265a71bf1eac8c942a0ccd
Evgenii Konkov <evgeniikonkov1@gmail.com>
2020-05-03 21:39:04 +0930

    hamburger menu and started adapt.des

---
+++ src/scripts/hamburger_menu.js
@@ -0,0 +1,44 @@
+function openmainmenu() {
+    const menubtn = document.querySelector('.hamburger__menu');
+    const menublock = document.querySelector('.menu__list');
+    const socialsList = document.querySelector('.socials__list')
+    const socials = document.querySelector('.socials');
+    const socialblock = document.createElement('li');
+
+    
+
+    menubtn.addEventListener('click', function() {
+
+        menubtn.classList.toggle('closed');
+        menublock.classList.toggle('active');
+        socials.classList.toggle('onmenu');
+        if (menubtn.classList.contains('closed')) {
+            menublock.appendChild(socialblock);
+            socialblock.appendChild(socialsList);
+        } else {
+            socials.appendChild(socialsList);
+            menublock.removeChild(socialblock);
+        }
+        
+    });
+
+    menublock.addEventListener('click', function(event) {
+
+        if (menublock.classList.contains('active') && event.target.classList.contains('header-menu__link')) {
+        
+        menubtn.classList.toggle('closed');
+        menublock.classList.toggle('active');
+        socials.classList.toggle('onmenu');
+        if (menubtn.classList.contains('closed')) {
+            menublock.appendChild(socialblock);
+            socialblock.appendChild(socialsList);
+        } else {
+            socials.appendChild(socialsList);
+            menublock.removeChild(socialblock);
+        }
+        };
+
+    });
+};
+
+openmainmenu();
