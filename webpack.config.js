427717cd9f1b8a315956c952d3737a88fe690270
Evgenii Konkov <evgeniikonkov1@gmail.com>
2020-06-14 13:31:27 +0930

    publicPath correction

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = 'https://github.com/EvanTechnology/portfolio/dist/';
+  const publicPath = './';
 
   const pcss = {
     test: /\.(p|post|)css$/,
